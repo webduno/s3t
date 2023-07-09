6436f8dd5bfe0bfceec199529d06f6d7fa899288
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-07-09 19:34:28 -0400

    Update ContextConnectedTable.tsx

--- src/module/inventory/ContextConnectedTable.tsx
+++ src/module/inventory/ContextConnectedTable.tsx
@@ -1,13 +1,43 @@
+"use client"
+import Dropdown from "@/dom/atom/common/Dropdown"
 import FlexTable from "@/dom/cell/form/FlexTable"
+import Link from "next/link"
 
 async function Component ({ initArray }:any) {
+    const buttonActionText = "..."
+    const buttonActionClass = "tx-lg pb-3 pt-1 px-2 tx-gray opaci-chov--25"
+    const actionCard = (slug:any) => {
+        const foundIndex =  initArray.findIndex((item:any)=>(item.slug == slug))+1
+    
+            return (<>
+              <Dropdown buttonTitle={buttonActionText} buttonClass={buttonActionClass}>
+                <div className="flex-col flex-align-stretch gap-1 bg-white pa-2 bord-r-8 z-100  ">
+                  {!!foundIndex && <>
+                    {!initArray[foundIndex-1].url &&<>No Link Found!</>}
+                    {!!initArray[foundIndex-1].url &&<>
+                      <Link  href={initArray[foundIndex-1].url || ""}
+                        className="duno-button-primary clickble nowrap"
+                        onClick={()=>{}}
+                      >
+                        Go to URL
+                      </Link>
+                      
+                    </>}
+                  </>}
+                </div>
+              </Dropdown>
+            </>)
+          }
+
+          
     return (
         <div>
-            <FlexTable theArray={initArray}
-                config={{idKey:"id",mainKey:"id",
+            <FlexTable theArray={initArray} bools={["isActionable"]} actionCard={actionCard}
+                config={{idKey:"slug",mainKey:"slug",idKeyTitle:"Slug",
                     childrenArray: [
-                        { key: "name", title: "Name"},
                         { key: "title", title: "Title"},
+                        { key: "desc", title: "Description"},
+                        { key: "status", title: "Status"},
                     ]
                 }}
             />
